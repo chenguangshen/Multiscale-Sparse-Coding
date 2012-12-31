6c897e93d38dca8344e8bfb30f2ae0b32fe27790
zayd <zayd.enam@gmail.com>
2012-12-31 14:35:20 -0500

    1024 pixel image

--- cvx_test/reconstruct.m
+++ cvx_test/reconstruct.m
@@ -1,27 +1,36 @@
-N = 4;
-M = 9; 
+% Total image pixels
+R = 1024;
 
-R = 16; 
+% Image height
+H = sqrt(R);
+
+% Max pixels per basis function
+N = 4;
+% Basis Function height
+HB = sqrt(N);
+
+% Number of basis functions
+M = (sqrt(R)-1)^2; 
 
 a = ones(N,M);
 s = ones(M,1);
 I = zeros(R,1);
 
-b = reshape(a, 2, 2, M);
-
 for i=1:M
 	%current column
-	j = ((i-1)*N)+1;
+	j = ((i-1)*H)+1;
 
-	if (j+5 > R)
+	if (j+H+1 > R)
 		q = j;
-		j = mod(j,11);
+		j = mod(j,R-(H+1));
 	end
 
+	% last square in bottom right corner
 	if j == 0
-		j = ((3-1)*N)+3;;
+		j = ((H-2)*H)+(H-1);
 	end
 
-	I(j:j+1) = I(j:j+1) + b(:,1,i);
-	I(j+4:j+5) = I(j+4:j+5) + b(:,2,i);
+	I(j:j+1) = I(j:j+1) + a(1:2,i);
+	I(j+H:j+H+1) = I(j+H:j+H+1) + a(3:4,i);
+	%I(j+H+2:j+H+1) = I(j+H:j+H+1) + a(9:16,i);
 end
